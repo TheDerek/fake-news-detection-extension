6a80dc359d432805dc6889d1731c95bf2fada0ed
Derek <mail@derek.tech>
2018-04-13 19:13:31 +0100

    Chained promises in an effient manner

--- popup.js
+++ popup.js
@@ -1,16 +1,5 @@
 let changeColor = document.getElementById('changeColor');
 
-// chrome.storage.sync.get('color', function(data) {
-//     changeColor.style.backgroundColor = data.color;
-//     changeColor.setAttribute('value', data.color);
-// });
-
-// changeColor.onclick = function(element) {
-//     let color = element.target.value;
-//     chrome.tabs.executeScript(
-//         {code: 'document.body.style.backgroundColor = "' + color + '";'});
-// };
-
 let minimumWaitTime = 1500;
 let errorMsg = document.getElementById('error-msg');
 let loading = document.getElementById('loading');
@@ -34,8 +23,39 @@ function setError(msg) {
     errorMsg.textContent = msg;
 }
 
-function sleep (time) {
-    return new Promise((resolve) => setTimeout(resolve, time));
+function sleep (startTime, response) {
+    return new Promise((resolve) => {
+        let loadTime = (new Date().getTime() - startTime);
+        let waitTime = minimumWaitTime - loadTime;
+
+
+        setTimeout(() => resolve(response), waitTime)
+    });
+}
+
+function acceptResponse(response) {
+    return new Promise((resolve, reject) => {
+        if (response.status !== 200) {
+            reject(`Looks like there was a problem. Status Code: ${response.status}`)
+        } else {
+            resolve(response)
+        }
+    });
+}
+
+function parseData(data) {
+    if(!data['is_article']) {
+        setError('No article found.');
+        return
+    }
+
+    if (data['category'] === 'fake') {
+        setFakeNews()
+    }
+
+    if (data['category'] === 'real') {
+        setRealNews();
+    }
 }
 
 let startTime = new Date().getTime();
@@ -44,53 +64,12 @@ chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     let url = encodeURIComponent(tabs[0].url);
 
     fetch(`http://127.0.0.1:5000/get/${url}`)
-        .then(
-            function(response) {
-                let loadTime = (new Date().getTime() - startTime);
-                let waitTime = minimumWaitTime - loadTime;
-                waitTime = Math.max(0, waitTime);
-
-                // Wait a certain amount of time before revealing the answer for UX reasons
-                sleep(waitTime).then(() => {
-                    if (response.status !== 200) {
-                        console.log('Looks like there was a problem. Status Code: ' +
-                            response.status);
-                        return;
-                    }
-
-                    // Examine the text in the response
-                    response.json().then(function(data) {
-                        console.log(data);
-                        if(!data['is_article']) {
-                            setError('No article found.');
-                            return
-                        }
-
-                        if (data['category'] === 'fake') {
-                            setFakeNews()
-                        }
-
-                        if (data['category'] === 'real') {
-                            setRealNews();
-                        }
-                        //document.getElementById('response').textContent = data;
-                    });
-                });
-            }
-        )
-        .catch(function(err) {
+        .then(response => sleep(startTime, response))
+        .then(response => acceptResponse(response))
+        .then(response => response.json())
+        .then(data => parseData(data))
+        .catch(err => {
             console.log('Fetch Error :-S', err);
             setError(err);
         });
 });
-
-
-
-
-// let xhr = new XMLHttpRequest();
-//
-// xhr.open("GET", "https://jsonplaceholder.typicode.com/posts/1", false);
-// xhr.send();
-//
-// let result = xhr.responseText;
-// document.getElementById('response').textContent = result;
